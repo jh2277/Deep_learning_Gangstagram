6e787e4dd3811180c3815cfe120bc1db382164c8
Joohyuk Kwak <neworld239@gmail.com>
2022-01-04 13:22:04 +0900

    mainpage랑 walking_possibility 수정한거 올림

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import requests
+
+html = requests.get('https://search.naver.com/search.naver?query=날씨')
+#pprint(html.text)
+
+soup = bs(html.text, 'html.parser')
+
+data1 = soup.find('div', {'class':'detail_box'})
+pprint(data1)
+
+# data2 = data1.find('dd')
+# # pprint(data2)
+#
+# fine_dust = data2[0].find('span',{'class':'num'}).text
+# print(fine_dust)
+#
+# ultra_fine_dust = data2[1].find('span',{'class':'num'}).text
+# print(ultra_fine_dust)
+
+
+
 
 app = Flask(__name__)
 
