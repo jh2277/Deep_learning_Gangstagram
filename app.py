ea12fda4a9ee8fc6ec1dcc1d4dc366bba6a76147
Joohyuk Kwak <neworld239@gmail.com>
2022-01-18 17:11:05 +0900

    profile 이미지 업로드, 최신 게시물이 올라가게끔 메인페이지 수정, app.py (add_post 등 수정)

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d606b3eac19f07a593307b87cf5073306b9a48d66060b7acccdc1a2a4e408adf
-size 25764
+oid sha256:822355b5ebadc2943b389b5b6a7e1c5400966b699949620a3c316e5159f3b85b
+size 25795
